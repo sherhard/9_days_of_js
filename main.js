5b6614c330325a7e5e6746bc7440c39795758470
sherhard <goeasysher@gmail.com>
2023-11-10 23:55:22 +0800

    two days

---
+++ main.js
@@ -0,0 +1,14 @@
+// const newsSet= new Set();
+// console.log(newsSet);
+const newsSet= new Set();
+for(let newsSet=0; newsSet<=10; newsSet++){
+ 
+    console.log([newsSet])
+    
+}
+const conyrtys=['france','Russian', 'Kyrkyzhstan', 'America'];
+ const count= new Set(conyrtys)
+ console.log(count)
+
+//  console.log(conyrtys.delete('france'))
+ console.log(conyrtys.size)
